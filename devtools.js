dea3bfb9757a88dba41e6f0bea1c2d623b02a5bb
Tomasz Michalak <tomek.michalak@gmail.com>
2020-01-13 14:42:46 +0100

    Merge JS methods.

--- devtools.js
+++ devtools.js
@@ -1,17 +1,12 @@
-function filterNone() {
-  return NodeFilter.FILTER_ACCEPT;
-}
-
-function getKnotXNodes(rootElem) {
+let knotxNodes = function() {
+  let rootElem = document.body;
   const COMMENTS = [];
   const COMMENT_NODE_CODE = 8;
   const HTML_NODE_CODE = 1;
   // Fourth argument, which is actually obsolete according to the DOM4 standard, is required in IE 11
   const iterator = document.createNodeIterator(
       rootElem,
-      NodeFilter.SHOW_ALL,
-      filterNone,
-      false
+      NodeFilter.SHOW_ALL
   );
 
   let curNode;
@@ -40,18 +35,15 @@ function getKnotXNodes(rootElem) {
       }
     }
   }
-  return COMMENTS;
-}
 
-/**
- * Converts list of nodes from all comments to Fragments. Example inputs / outputs:
- * - [script, node] -> [ { script, node } ]
- * - [ script, node, script, node, node ] -> [ { script, node }, { script, node, node } ]
- */
-function getFragments(nodes) {
+  /**
+   * Converts list of nodes from all comments to Fragments. Example inputs / outputs:
+   * - [script, node] -> [ { script, node } ]
+   * - [ script, node, script, node, node ] -> [ { script, node }, { script, node, node } ]
+   */
   let fragments = [];
   let currentFragment = {};
-  nodes.forEach(function (node) {
+  COMMENTS.forEach(function (node) {
     if (node.type === "application/json") {
       currentFragment = {};
       currentFragment['debug'] = JSON.parse(node.innerText);
@@ -66,17 +58,13 @@ function getFragments(nodes) {
     }
   });
   return fragments;
-}
-
-let page_getProperties = function () {
-  const nodes = getKnotXNodes(document.body);
-  return getFragments(nodes);
 };
 
 chrome.devtools.panels.elements.createSidebarPane("Knot.x Fragments",
     function (sidebar) {
+
       function updateElementProperties() {
-        sidebar.setExpression("(" + page_getProperties.toString() + ")()");
+        sidebar.setExpression("(" + knotxNodes.toString() + ")()");
       }
 
       updateElementProperties();
