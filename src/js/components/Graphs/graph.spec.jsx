f1b88e92b376ad7dc461ef633b19c3af14a048a0
SzajekSzymon <szajekszymon.personal@gmail.com>
2020-03-10 11:06:08 +0100

    #33 | CR fixes

---
+++ src/js/components/Graphs/graph.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, mount } from 'enzyme';
+import GraphComponent from './graph';
+import {
+  GraphContainer,
+  Graph,
+} from './graph.style';
+import { singleNode } from '../../helpers/graph/declarationHelper.mock';
+
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('A suite', () => {
+  it('should render without throwing an error', () => {
+    expect(shallow(<GraphComponent />).contains(
+      <GraphContainer className="graphContainer">
+        <Graph className="graph" />
+      </GraphContainer>,
+    )).toBe(true);
+  });
+
+  // TODO test for correct rendering graph.
+  // it('should render  throwing an error', () => {
+  //   expect(mount(<GraphComponent graphJson={singleNode} />).find('.vis-network')).toHaveLength(1);
+  // });
+});
