cd7fc1e422440a7ad36a00df8f3e67a9cd9da301
SzajekSzymon <szajekszymon.personal@gmail.com>
2020-03-23 15:37:24 +0100

    rename files

--- src/js/components/Graphs/Legend/legend.spec.jsx
+++ src/js/components/Graphs/Legend/legend.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import LegendSection from './legendSection';
+import LegendSection from './LegendSection';
 import { legendArraysMock } from './legend.mock';
 import {
   LegendHeader,
