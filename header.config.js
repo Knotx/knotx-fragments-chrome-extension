5639ac2a3cba0397f59a1b6292f42b0381eb7950
SzajekSzymon <szajekszymon.personal@gmail.com>
2020-04-01 11:51:55 +0200

    add eslint rule and add missing header

--- header.config.js
+++ header.config.js
@@ -13,9 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import styled from 'styled-components';
-
-export const Timeline = styled.div`
-  height: 100%;
-`;
