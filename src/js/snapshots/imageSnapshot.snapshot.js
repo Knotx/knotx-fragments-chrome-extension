4987a27600020caf0fed6820e17c061834d163ea
SzajekSzymon <szajekszymon.personal@gmail.com>
2021-01-05 13:12:38 +0100

    wait before making snapshot

--- src/js/snapshots/imageSnapshot.snapshot.js
+++ src/js/snapshots/imageSnapshot.snapshot.js
@@ -20,7 +20,7 @@
 
 const snapshot = async (url) => {
   await page.goto(url, { waitUntil: 'load' });
-  page.waitForTimeout(3000);
+  await page.waitForTimeout(3000);
 
   return page.screenshot();
 };
