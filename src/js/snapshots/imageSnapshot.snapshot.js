74d2d10ae8a9488ed56c74c2ac27f2a855f6ff30
SzajekSzymon <szajekszymon.personal@gmail.com>
2021-01-05 13:20:00 +0100

    update snapshots

--- src/js/snapshots/imageSnapshot.snapshot.js
+++ src/js/snapshots/imageSnapshot.snapshot.js
@@ -20,7 +20,7 @@
 
 const snapshot = async (url) => {
   await page.goto(url, { waitUntil: 'load' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(2000);
 
   return page.screenshot();
 };
